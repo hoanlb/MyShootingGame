67d95752add98a8803cf1e14c087f07ae6ead4f4
Lâm Bình Hoàn <hoanlb1988@gmail.com>
2024-05-28 19:02:51 +0700

    update

--- Assets/Scripts/PointUpdate.cs
+++ Assets/Scripts/PointUpdate.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 
 public class PointUpdate : MonoBehaviour
 {
-    public int point = 0;
-    public TextMeshProUGUI pointNumber;
-
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Army")
         {
+            Debug.Log("AddPoint 5");
             GameCommon.Instance.AddPoint(5);
             Destroy(gameObject);
         }
 
         if (coll.gameObject.tag == "Bom")
         {
+            Debug.Log("AddPoint 15");
             GameCommon.Instance.AddPoint(15);
             Destroy(gameObject);
         }
